ea0bd8e08bf47f10146009385f23e48579ae77e8
faber <faber@hivesys.com.br>
2018-08-21 22:09:50 -0300

    style: add bootstrap to home page

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,23 @@ import '../styles/App.css';
 
 class App extends Component {
   render() {
-    let token = localStorage.token
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Token = {token}
-        </p>
+      <div>
+        <nav class="navbar navbar-dark bg-dark">
+          <a className="navbar-brand company-name" href="http://localhost:3000">
+            <img src={logo} className="App-logo d-inline-block align-top" width="60" alt="logo" />
+            ReactForum
+          </a>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search Category" aria-label="Search" />
+            <button class="btn btn-info my-2 my-sm-0" type="submit">Search </button>
+          </form>
+        </nav>
       </div>
     );
   }
